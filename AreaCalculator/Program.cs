2c3f65b65d9470057d3a076b881eb3b47e822d99
siuupa <patryksiuda@icloud.com>
2021-12-06 23:47:41 +0100

    written methods without actual result

--- AreaCalculator/Program.cs
+++ AreaCalculator/Program.cs
@@ -2,8 +2,31 @@
 
 namespace AreaCalculator
 {
-    class Program
+    public class Program
     {
+        public int AreaOfTriangle(int a, int h)
+        {
+            return 0;
+        }
 
+        public int AreaOfSquare(int a)
+        {
+            return 0;
+        }
+
+        public int AreaOfRectangle(int a, int b)
+        {
+            return 0;
+        }
+
+        public int AreaOfTrapeze(int a, int b, int h)
+        {
+            return 0;
+        }
+
+        public int AreaOfCircle(int r)
+        {
+            return 0;
+        }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,13 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AreaCalculator;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Program program;
+
+        [TestInitialize]
+        public void Setup()
         {
+            program = new Program();
         }
     }
 }
