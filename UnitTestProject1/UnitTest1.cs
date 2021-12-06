1b46275721b192c1bbab86223e778112f137d2a7
siuupa <patryksiuda@icloud.com>
2021-12-06 23:59:22 +0100

    written test methods in UnitTest1.cs file, tests are failing

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -11,7 +11,58 @@ namespace UnitTestProject1
         [TestInitialize]
         public void Setup()
         {
+            //Arrange
             program = new Program();
+        }
+
+        [TestMethod]
+        public void AreaOfTriangle_CalculateAreaOfTringleGivenTwoArguments_ShouldReturnCorrectArea()
+        {
+            //Act
+            var result = program.AreaOfTriangle(a: 5, h: 3);
+
+            //Assert
+            Assert.AreEqual(7.5, result);
+        }
+
+        [TestMethod]
+        public void AreaOfSquare_CalculateAreaOfSquareGivenOneArgument_ShouldReturnCorrectArea()
+        {
+            //Act
+            var result = program.AreaOfSquare(a: 5);
+
+            //Assert
+            Assert.AreEqual(25, result);
+        }
+
+        [TestMethod]
+        public void AreaOfRectangle_CalculateAreaOfRectangleGivenTwoArguemnts_ShouldReturnCorrectArea()
+        {
+            //Act
+            var result = program.AreaOfRectangle(a: 10, b: 8);
+
+            //Assert
+            Assert.AreEqual(80, result);
+        }
+
+        [TestMethod]
+        public void AreaOfTrapeze_CalculateAreaOfTrapezeGivenThreeArguments_ShouldReturnCorrectArea()
+        {
+            //Act
+            var result = program.AreaOfTrapeze(a: 5, b: 10, h: 8);
+
+            //Assert
+            Assert.AreEqual(200, result);
+        }
+
+        [TestMethod]
+        public void AreaOfCircle_CalculateAreaOfCircleGivenOneArgument_ShouldReturnCorrectArea()
+        {
+            //Act
+            var result = program.AreaOfCircle(r: 5);
+
+            //Assert
+            Assert.AreEqual(78.5, result);
         }
     }
 }
